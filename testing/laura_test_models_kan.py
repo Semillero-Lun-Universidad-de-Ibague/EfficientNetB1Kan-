1050135fed6664f29fe5d25f4c96e99195b5699e
lurr98 <laurazeidler.konstanz@gmaio.com>
2024-10-17 17:48:45 -0500

    added and updated json files + fixed json creation in script

--- testing/laura_test_models_kan.py
+++ testing/laura_test_models_kan.py
@@ -10,6 +10,7 @@ from common_testing import test_model
 from utils import get_data_from_json
 from models.laura_vgg_kan import VGG16_KAN
 from models.laura_resnext_kan import ResNext_KAN
+from models.efficientnet_kan import EfficientNetB1_KAN
 
 NAME_JSON_FILE = 'data_experiment_params.json'
 
@@ -19,7 +20,7 @@ common_testing.batch_size = 210
 
 if __name__ == '__main__':
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    torch.cuda.set_device(1)
+    torch.cuda.set_device(0)
 
     parser = argparse.ArgumentParser(description='Run this script in order to perform hyperparameter tuning on the desired model.')
     parser.add_argument('model_name', type=str,
@@ -31,28 +32,31 @@ if __name__ == '__main__':
     params_nni = nni.get_next_parameter()
 
     params = {
-        'grid_size': params_nni.get('grid_size', 64),
-        'spline_order': params_nni.get('spline_order', 3),
-        'scale_noise': params_nni.get('scale_noise', 0.97),
-        'scale_base': params_nni.get('scale_base', 0.97),
-        'scale_spline': params_nni.get('scale_spline', 0.97)
+        'grid_size': params_nni.get('grid_size', 32),
+        'spline_order': params_nni.get('spline_order', 2),
+        'scale_noise': params_nni.get('scale_noise', 0.66),
+        'scale_base': params_nni.get('scale_base', 0.6),
+        'scale_spline': params_nni.get('scale_spline', 0.62)
     }
 
     # TODO: continue here, add significance tests       
     if args.model_name == 'vgg':
         vgg = vgg16(pretrained=True)
-        test_model(vgg, 'VGG16_10epochs_{}'.format(date.today()), num_epochs=1, progress_bar=False)
+        test_model(vgg, 'VGG16_10epochs_{}'.format(date.today()), num_epochs=10, progress_bar=False)
     elif args.model_name == 'vgg_kan':
         model = VGG16_KAN(4, params)
-        test_model(model, 'VGG16_KAN_10epochs_{}'.format(date.today()), num_epochs=1, progress_bar=False)
+        test_model(model, 'VGG16_KAN_10epochs_{}'.format(date.today()), num_epochs=10, progress_bar=False)
     elif args.model_name == 'resnext':
         resnext = resnext50_32x4d(pretrained=True)
-        test_model(resnext, 'ResNext_10epochs_{}'.format(date.today()), num_epochs=1, progress_bar=False)
+        test_model(resnext, 'ResNext_10epochs_{}'.format(date.today()), num_epochs=10, progress_bar=False)
     elif args.model_name == 'resnext_kan':
         model = ResNext_KAN(4, params)
-        test_model(model, 'ResNext_KAN_10epochs_{}'.format(date.today()), num_epochs=1, progress_bar=False)
+        test_model(model, 'ResNext_KAN_10epochs_{}'.format(date.today()), num_epochs=10, progress_bar=False)
     elif args.model_name == 'efficientnet':     
         model = efficientnet_b1(pretrained=True)
-        test_model(model, 'EfficientNet_10epochs_{}'.format(date.today()), num_epochs=1, progress_bar=False)
+        test_model(model, 'EfficientNet_10epochs_{}'.format(date.today()), num_epochs=10, progress_bar=False)
+    elif args.model_name == 'efficientnet_kan':     
+        model = EfficientNetB1_KAN(4, params)
+        test_model(model, 'EfficientNet_10epochs_kan{}'.format(date.today()), num_epochs=10, progress_bar=False)
 
 
